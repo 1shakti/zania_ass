60728bba7654567d7cb837bfe531c3b838b0e8fe
1shakti <shakti26.sj@gmail.com>
2024-09-06 18:36:22 +0530

    responsive ui

--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -15,7 +15,7 @@ function CardList({ cards, onClickCard }: CardProps) {
         <div
           ref={provided.innerRef}
           {...provided.droppableProps}
-          className="grid grid-cols-3 gap-4"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
         >
           {cards.map((card, index) => (
             <Draggable
