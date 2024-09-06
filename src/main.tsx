0670379f24aaa15a44c2c06798f87999b80fda49
1shakti <shakti26.sj@gmail.com>
2024-09-06 17:42:22 +0530

    small change

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Start MSW before rendering the app
 async function enableMocking() {
-    if (process.env.NODE_ENV !== 'development') {
-      return;
-    }
     const { worker } = await import('./mocks/browser');
     return worker.start();
   }
