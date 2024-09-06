8a08dd6cdb24554a3c9b582bbd1eeba1c3e87010
1shakti <shakti26.sj@gmail.com>
2024-09-06 18:32:21 +0530

    small ui fix

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 interface TimerProps {
   lastSavedTime: number | null;
 }
 
-function Timer({ lastSavedTime }:TimerProps){
-  const [timeElapsed, setTimeElapsed] = useState<string>('Not saved yet');
+function Timer({ lastSavedTime }: TimerProps) {
+  const [timeElapsed, setTimeElapsed] = useState<string>("Not saved yet");
 
   useEffect(() => {
     const calculateTimeElapsed = () => {
-        if (lastSavedTime) {
-          const now = Date.now();
-          const secondsSinceLastSave = Math.floor((now - lastSavedTime) / 1000);
-          const minutesSinceLastSave = Math.floor(secondsSinceLastSave / 60);
-          const hoursSinceLastSave = Math.floor(minutesSinceLastSave / 60);
-  
-          const seconds = secondsSinceLastSave % 60;
-          const minutes = minutesSinceLastSave % 60;
-          const hours = hoursSinceLastSave;
-  
-          const formattedTime = `${hours} hours ${minutes} minutes ${seconds} seconds ago`;
-          setTimeElapsed(formattedTime);
-        } else {
-          setTimeElapsed('Not saved yet');
-        }
-      };
+      if (lastSavedTime) {
+        const now = Date.now();
+        const secondsSinceLastSave = Math.floor((now - lastSavedTime) / 1000);
+        const minutesSinceLastSave = Math.floor(secondsSinceLastSave / 60);
+        const hoursSinceLastSave = Math.floor(minutesSinceLastSave / 60);
+
+        const seconds = secondsSinceLastSave % 60;
+        const minutes = minutesSinceLastSave % 60;
+        const hours = hoursSinceLastSave;
+
+        const formattedTime = `${hours} hours ${minutes} minutes ${seconds} seconds ago`;
+        setTimeElapsed(formattedTime);
+      } else {
+        setTimeElapsed("Not saved yet");
+      }
+    };
 
     calculateTimeElapsed();
     const intervalId = setInterval(calculateTimeElapsed, 1000);
@@ -32,7 +32,12 @@ function Timer({ lastSavedTime }:TimerProps){
     return () => clearInterval(intervalId);
   }, [lastSavedTime]);
 
-  return <span className='inline-block mb-8'><span className='font-bold'>Since last saved: </span>{timeElapsed}</span>;
-};
+  return (
+    <span>
+      <span className="font-bold">Since last saved: </span>
+      {timeElapsed}
+    </span>
+  );
+}
 
 export default Timer;
--- src/containers/CardContainer.tsx
+++ src/containers/CardContainer.tsx
@@ -7,19 +7,19 @@ import { ImSpinner2 } from "react-icons/im";
 import Timer from "../components/Timer/Timer";
 
 function CardContainer() {
-  const { cards, setCards, onDragEnd, isSaving, lastSavedTime } = useDragAndDrop();
+  const { cards, setCards, onDragEnd, isSaving, lastSavedTime } =
+    useDragAndDrop();
   const [SelectedImage, setSelectedImage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchCards = async () => {
-      try{
-        const response = await fetch('/api/products');
+      try {
+        const response = await fetch("/api/products");
         const data = await response.json();
         setCards(data);
-      }catch(e){
-        console.log("error",e)
+      } catch (e) {
+        console.log("error", e);
       }
-
     };
     fetchCards();
   }, [setCards]);
@@ -34,16 +34,18 @@ function CardContainer() {
 
   return (
     <>
-      {isSaving && (
-          <div className="saving-indicator">
-            <ImSpinner2 className="animate-spin text-gray-500 text-3xl" />
-            Saving data... 
+      <div className="flex items-center mr-3 mb-8">
+        <Timer lastSavedTime={lastSavedTime} />
+        {isSaving && (
+          <div className="saving-indicator flex items-center ml-3">
+            <ImSpinner2 className="animate-spin text-gray-500 text-2xl mr-2" />
+            Saving data...
           </div>
         )}
-        <Timer lastSavedTime={lastSavedTime} />
-        <DragDropContext onDragEnd={onDragEnd}>
-          <CardList cards={cards} onClickCard={handleCardClick} />
-        </DragDropContext>
+      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <CardList cards={cards} onClickCard={handleCardClick} />
+      </DragDropContext>
       {SelectedImage && (
         <CardOverlay image={SelectedImage} onClose={handleOverlayClose} />
       )}
